f5bdac107397e092e034b4df5069feeba3789d41
TieByn26 <dxuantienn@gmail.com>
2024-06-27 21:36:03 +0700

    chapter 3

---
+++ js/Chapter3/number.js
@@ -0,0 +1,28 @@
+//integer
+let integer = 42;
+console.log(integer); // Output: 42
+//float
+let float = 3.14;
+console.log(float); // Output: 3.14
+//EN
+let exponential = 1.23e4;
+console.log(exponential); // Output: 12300
+//hex
+let hex = 0x1a;
+console.log(hex); // Output: 26
+//binary
+let binary = 0b1010;
+console.log(binary); // Output: 10
+//octal
+let octal = 0o52;
+console.log(octal); // Output: 42
+// Special Numeric Values
+// Infinity
+let infinity = Infinity;
+console.log(infinity); // Output: Infinity
+// Negative Infinity
+let negativeInfinity = -Infinity;
+console.log(negativeInfinity); // Output: -Infinity
+//NaN (Not-a-Number)
+let notANumber = NaN;
+console.log(notANumber); // Output: NaN
---
+++ js/Chapter3/text.js
@@ -0,0 +1,18 @@
+//Single Quotes
+let singleQuoteString = 'Hello, world!';
+console.log(singleQuoteString); // Output: Hello, world!
+//Double Quotes
+let doubleQuoteString = "Hello, world!";
+console.log(doubleQuoteString); // Output: Hello, world!
+//Backticks (Template Literals)
+let name = 'John';
+let templateLiteral = `Hello, ${name}!`;
+console.log(templateLiteral); // Output: Hello, John!
+//Multiline Strings (using Template Literals)
+let multilineString = `This is a
+multiline string.`;
+console.log(multilineString);//Output:This is amultiline string.
+//Escaping Characters
+let escapingCharacters = 'It\'s a beautiful day!';
+console.log(escapingCharacters);
+//String Concatenation
